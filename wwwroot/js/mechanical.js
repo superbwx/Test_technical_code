97b18431332d03fc7344c093d1369d49c767c8bc
DESKTOP-HI81P4C\Arief <superbwx@gmail.com>
2023-03-14 11:21:36 +0700

    Update

--- wwwroot/js/mechanical.js
+++ wwwroot/js/mechanical.js
@@ -17,6 +17,7 @@ function prima() {
 }
 
 function callData(number, type) {
+  document.getElementById("hasilKalkulasi").innerHTML = "";
   $.ajax({
     type: "GEt",
     url: type + "/" + number,
@@ -24,9 +25,28 @@ function callData(number, type) {
     dataType: "json",
     success: function (data) {
       console.log(data);
+      if (type == "segitiga") {
+        showSegitiga(data);
+      } else {
+        showDeret(data, "Bilangan " + type);
+      }
     },
     error: function (e) {
       console.log("Gagal");
     },
   });
+}
+
+function showSegitiga(data) {
+  var display = "";
+  document.getElementById("hasilKalkulasi").innerHTML += "Bentung <br>";
+  for (i = 0; i < data.length; i++) {
+    display += "<div>" + data[i] + "</div>";
+  }
+  document.getElementById("hasilKalkulasi").innerHTML += display;
+}
+
+function showDeret(data, type) {
+  document.getElementById("hasilKalkulasi").innerHTML += type + "<br>";
+  document.getElementById("hasilKalkulasi").innerHTML += data.toString();
 }
